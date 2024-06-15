dd3aef00eeaf004a65a1952ce5a011e365e3ae3f
Julian Berman <Julian@GrayVines.com>
2024-06-15 19:10:51 +0300

    Scaffolding.

---
+++ noxfile.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import nox
+
+ROOT = Path(__file__).parent
+REQUIREMENTS = dict(
+    main=ROOT / "requirements.txt",
+)
+REQUIREMENTS_IN = [  # this is actually ordered, as files depend on each other
+    (path.parent / f"{path.stem}.in", path) for path in REQUIREMENTS.values()
+]
+
+SUPPORTED = ["3.12"]
+LATEST = SUPPORTED[-1]
+
+nox.options.default_venv_backend = "uv|virtualenv"
+nox.options.sessions = []
+
+
+def session(default=True, python=LATEST, **kwargs):  # noqa: D103
+    def _session(fn):
+        if default:
+            nox.options.sessions.append(kwargs.get("name", fn.__name__))
+        return nox.session(python=python, **kwargs)(fn)
+
+    return _session
+
+
+@session(default=False)
+def requirements(session):
+    """
+    Update the project's pinned requirements.
+
+    You should commit the result afterwards.
+    """
+    if session.venv_backend == "uv":
+        cmd = ["uv", "pip", "compile"]
+    else:
+        session.install("pip-tools")
+        cmd = ["pip-compile", "--resolver", "backtracking", "--strip-extras"]
+
+    for each, out in REQUIREMENTS_IN:
+        # otherwise output files end up with silly absolute path comments...
+        relative = each.relative_to(ROOT)
+        session.run(*cmd, "--upgrade", "--output-file", out, relative)
